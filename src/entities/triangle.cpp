80dfcecc584b517d14835060b07f610c61233fa1
astral-shining <astral-shining@users.noreply.github.com>
2022-07-20 17:51:08 +0200

    Fixes

--- src/entities/triangle.cpp
+++ src/entities/triangle.cpp
@@ -53,21 +53,21 @@ void Triangle::move() {
     if (input.getKey(KEY_A)) {
         position.x -= delta_time * velocity;
         rotation = 0;
-        scale.x = -1;
+        scale.x = abs(scale.x) * -1;
     }
     if (input.getKey(KEY_D)) {
         position.x += delta_time * velocity;
         rotation = 0;
-        scale.x = 1;
+        scale.x = abs(scale.x);
     }
     if (input.getKey(KEY_W)) {
         position.y += delta_time * velocity;
-        scale.x = 1;
+        scale.x = abs(scale.x);
         rotation = 90;
     }
     if (input.getKey(KEY_S)) {
         position.y -= delta_time * velocity;
-        scale.x = 1;
+        scale.x = abs(scale.x);
         rotation = -90;
     }
     if (input.getKey(KEY_LEFT)) {
